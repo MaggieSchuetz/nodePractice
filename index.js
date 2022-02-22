cf7f9d536fbadf2809d760557b64dc6ea7a24038
Magdalena Schuetz <m.schuetz@outlook.de>
2022-02-22 09:39:16 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const writeFile = require('./writeFile.js');
+
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input: input, output: output });
+
+rl.question('What is the name of your function? ', (answer) => {
+  writeFile(answer);
+  console.log(answer);
+
+  rl.close();
+});
